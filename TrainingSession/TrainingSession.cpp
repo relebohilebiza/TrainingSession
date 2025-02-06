1e7fe3d445168a7e59f6c62123ae2c10066c4b2a
ishmael Biza <33324210+relebohilebiza@users.noreply.github.com>
2025-02-06 14:34:54 +0200

    Add project files.

---
+++ TrainingSession/TrainingSession.cpp
@@ -0,0 +1,12 @@
+﻿// TrainingSession.cpp : Defines the entry point for the application.
+//
+
+#include "TrainingSession.h"
+
+using namespace std;
+
+int main()
+{
+	cout << "Hello CMake." << endl;
+	return 0;
+}
