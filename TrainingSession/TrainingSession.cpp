4f4611fdfa445b849e0404aa8c5022bc7a274a2c
ishmael Biza <33324210+relebohilebiza@users.noreply.github.com>
2025-02-08 18:14:45 +0200

    Continue to learn
    rand(), randn () and randint()

--- TrainingSession/TrainingSession.cpp
+++ TrainingSession/TrainingSession.cpp
@@ -19,7 +19,7 @@ int main()
 	* They help in defining transformation matrices in computer vision
 	*/
 	auto eyeTensor = torch::eye(4); // A tensor with 4 rows and 4 colunms, this code will create  4 x 4 tensor.
-	std::cout << "eye Tensor: \n" << eyeTensor << std::endl; // [ [1,0,0,0], [0,1,0,0],[0,0,1,0],[0,0,0,1]]
+	std::cout << "eye Tensor: \n" << eyeTensor << std::endl; // [[1,0,0,0], [0,1,0,0],[0,0,1,0],[0,0,0,1]]
 
 	/* 
 	* Function full produces a tensor of the specified value and size
@@ -29,6 +29,19 @@ int main()
 
 	auto simpleTensor = torch::tensor({ 33,22,11 });// A tensor with 3 rows.
 	std::cout << "A simple Tensor:\n" << simpleTensor << std::endl; // [33, 22, 11]
+
+
+	/* ----------- Random intitialization ------------------- */
+
+	auto randTensor = torch::rand({3, 4}); // rand generates a random value between 0 -1, A tensor with 3 rows and 4 colunms, this code will create  3 x 4 tensor.
+	std::cout << "rand Tensor: \n" << randTensor << std::endl;// [[0.6400, 0.2377, 0.2625, 0.9444],[0.7461, 0.2347 , 0.1737, 0.5441],[0.5214, 0.6729, 0.7972, 0.8869]]
+
+	auto randnTensor = torch::randn({2,2}); // randn takes th random value of the normal distribution N(0,1), A tensor with 2 rows and 2 colunms, this code will create 2 X 2 tensor.
+	std::cout << "randn Tensor: \n" << randnTensor << std::endl; // [[ 1.3227, 1.1869],[1.0160, -1.7203]]
+
+	auto randIntTensor = torch::randint(0, 4,{2,2}); // randint takes the random integer value of min , max. A tensor with 2 rows and 2 colums, this code will create 2 x 2 tensor.
+	std::cout << "randint Tensor: \n" << randIntTensor << std::endl; //[[0 , 3], [1, 3]]
+
 
 	return 0;
 }
